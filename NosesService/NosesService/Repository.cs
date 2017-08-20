ee94f380ea35acfee505c0e97c0faa7c82733d3b
Васо Джинчвелашвили <vvdzhinchvelashvili@edu.hse.ru>
2017-08-20 13:35:31 +0300

    commit

---
+++ NosesService/NosesService/Repository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NosesService.Data;
+
+namespace NosesService
+{
+    public class Repository
+    {
+        private readonly NosesDbSecondEntities _context;
+
+        public Repository()
+        {
+            _context = new NosesDbSecondEntities();
+        }
+        public void Seed()
+        {
+            var kek = new Random();
+            _context.Users.Add(new User(
+            {
+                create_date = DateTime.Now.AddDays(-kek.Next(1,100)),
+                
+            }))
+        }
+
+    }
+}
